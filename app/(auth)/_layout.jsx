934d160b503983cbd894f9ae62ae4b0dc0704c24
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-07-16 22:50:51 +0800

    create boilerplate

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  );
+};
+
+export default AuthLayout;
---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pbold">Aora</Text>
+      <StatusBar style="auto" />
+      <Link href="/Profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
