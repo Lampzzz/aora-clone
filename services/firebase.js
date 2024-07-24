536c3faa06675dc115290ac706b2c5a36673544e
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-07-24 16:07:47 +0800

    Update firebase.js

--- services/firebase.js
+++ services/firebase.js
@@ -1,18 +1,11 @@
-import { initializeApp } from "@firebase/app";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "@firebase/app";
+import { getFirestore } from "@firebase/firestore";
 import {
   getAuth,
   initializeAuth,
   getReactNativePersistence,
 } from "@firebase/auth";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  getDocs,
-  where,
-} from "@firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBLNA3jbw2O1UvKBDlMGtLf9IWeMyYqMno",
@@ -26,10 +19,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
+// const auth = getAuth(app);
 
-// const auth = initializeAuth(app, {
-//   persistence: getReactNativePersistence(AsyncStorage),
-// });
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 
-export { app, db, auth, collection, addDoc, query, getDocs, where };
+export { app, db, auth };
