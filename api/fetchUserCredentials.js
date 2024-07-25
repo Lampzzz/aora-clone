e3801085b70249f1a7bb201300f31caddd2712ba
Lampzzz <lampazaj@gmail.com>
2024-07-25 15:54:20 +0800

    Update fetchUserCredentials.js

--- api/fetchUserCredentials.js
+++ api/fetchUserCredentials.js
@@ -1,4 +1,5 @@
 import { collection, query, where, getDocs } from "@firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { db } from "../services/firebase";
 
 const fetchUserCredentials = async (uid) => {
@@ -13,7 +14,10 @@ const fetchUserCredentials = async (uid) => {
     }
 
     const userDoc = querySnapshot.docs[0];
-    return userDoc.data();
+    const data = userDoc.data();
+    await AsyncStorage.setItem("userCredentials", JSON.stringify(data));
+
+    return data;
   } catch (error) {
     console.error("Error fetching user credentials:", error.message);
     throw error;
