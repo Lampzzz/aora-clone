2139eb71630ee890efd3920e2d33485691d39e35
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-09-03 10:58:52 +0800

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,28 +1,21 @@
 import { initializeApp } from "@firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "@firebase/storage";
-import {
-  getAuth,
-  initializeAuth,
-  getReactNativePersistence,
-} from "@firebase/auth";
+import { getAuth } from "@firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLNA3jbw2O1UvKBDlMGtLf9IWeMyYqMno",
-  authDomain: "aora-17.firebaseapp.com",
-  projectId: "aora-17",
-  storageBucket: "aora-17.appspot.com",
-  messagingSenderId: "1003941264176",
-  appId: "1:1003941264176:android:1514354ae63c98c26f26f5",
-  measurementId: "",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
-// const auth = initializeAuth(app, {
-//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-// });
 
 export { app, db, storage, auth };
